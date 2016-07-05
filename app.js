f5f7725d3a63ca8e1b242fbb162dc19ada074729
David White <davidwhite@El-Duderino.local>
2016-07-05 09:01:04 -0600

    updates to last loop and to clear both input fields after event

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function addClass(){
     classes.push(newClass);
   }
   addToSelect(document.getElementById('availableClasses'), newClass);
+  document.getElementById('class').value = '';
   return false;
 }
 
@@ -16,34 +17,41 @@ function addStudent(){
   if(newStudent.length) {
     students.push(newStudent);
   }
-  addToSelect(document.getElementById('registeredStudents'), newStudent); 
+  addToSelect(document.getElementById('registeredStudents'), newStudent);
+  document.getElementById('student').value = ''; 
   return false;
 }
 
 function addToSelect(menu, value) {
+  
   menu.options[menu.options.length] = new Option(value, value);
 }
 
 function register(){
   var className = document.getElementById('availableClasses').value;  
-  var student = document.getElementById('registeredStudents').value;  
-  
-  schedule.push({
-    'class': className,
-    'student': student
-  });
+  var student = document.getElementById('registeredStudents').value;
+
+      schedule.push({
+        'class': className,
+        'student': student
+      });
+
+    
   
   console.log(schedule);
   setSchedule();
+
 }
 
 function setSchedule() {
   var html = '';
-  
-  for(var i = 0; i < schedule.length; i++) {
-    html += html + schedule[i].class + ' - ' + schedule[i].student + '<br />'
-  }
-  
+
+  console.log(html);
+  for(var i = 0; i < schedule.length; i++) {    
+    html += schedule[i].class + ' Is the Subject You are enrolled in ' + schedule[i].student + '<br />'
+ 
+}
+  console.log(html);
   document.getElementById('schedule').innerHTML = html;
 }
 
